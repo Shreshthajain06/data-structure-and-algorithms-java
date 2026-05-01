cf0985a131b5a6637ec36fe18b82c6052a26b455
Shreshtha Jain <shreshthajain46@gmail.com>
2026-05-01 23:44:29 +0530

    Add files via upload

---
+++ duplicate2.java
@@ -0,0 +1,33 @@
+import java.util.HashMap;
+
+public class duplicate2 {
+  public static boolean indexCheck(int[] nums,int k){
+    HashMap<Integer,Integer> map=new HashMap<>();
+    
+        for (int i = 0; i < nums.length; i++) {
+
+            if (map.containsKey(nums[i])) {
+
+                int prevIndex = map.get(nums[i]);
+
+                if (i - prevIndex <= k) {
+                    return true;
+                }
+            }
+
+            map.put(nums[i], i);
+        }
+
+        return false;
+  }
+
+
+
+
+    public static void main(String[] args) {
+        int[] nums={1,2,3,1};
+        int k=3;
+        boolean ans=indexCheck(nums, k);
+        System.out.println(ans);
+    }
+}
