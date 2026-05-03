e2778de636963e1418f8193ca97d253893469d34
Shreshthajain06 <shreshthajain46@gmail.com>
2026-05-03 23:55:54 +0530

    Validanagram solved

---
+++ Validanagram.java
@@ -0,0 +1,38 @@
+import java.util.HashMap;
+
+public class Validanagram {
+    public static boolean  checkAnagram(String s,String t){
+    if(s.length()!=t.length()){
+            return false;
+        }
+        HashMap<Character,Integer> map=new HashMap<>();
+        for(int i=0;i<s.length();i++){
+             char ch=s.charAt(i);
+             if(map.containsKey(ch)){
+                map.put(ch,map.get(ch)+1);
+             }
+             else{
+                map.put(ch,1);
+             }
+        }
+        for(int i=0;i<t.length();i++){
+            char ch=t.charAt(i);
+            if(!map.containsKey(ch) || map.get(ch)==0){
+                return false;
+            }
+            else{
+                map.put(ch,map.get(ch)-1);
+            }
+        }return true;
+    }
+
+    
+    
+    public static void main(String[] args) {
+        String s="abcde";
+        String t="bcdae";
+       System.out.println(checkAnagram(s,t));
+      
+    }
+}
+
